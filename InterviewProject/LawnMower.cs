e307819b6798ac1237f13498f68c86b50b96fadf
Tamás Fodor <fodor.tamas48@gmail.com>
2022-11-05 12:06:53 +0100

    Created LawnMower class with properties and constructor.

---
+++ InterviewProject/LawnMower.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InterviewProject
+{
+    internal class LawnMower
+    {
+        int positionX;
+        int positionY;
+
+        char lawnMowerChar;
+
+        public int PositionX { get => positionX; set => positionX = value; }
+        public int PositionY { get => positionY; set => positionY = value; }
+        public char LawnMowerChar { get => lawnMowerChar; set => lawnMowerChar = value; }
+
+        public LawnMower(int positionX, int positionY)
+        {
+            this.positionX = positionX;
+            this.positionY = positionY;
+            this.lawnMowerChar = 'M';
+        }
+    }
+}
