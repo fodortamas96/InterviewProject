1062b64471dd54834ef5ccba4db4a3330b7de553
Tamás Fodor <fodor.tamas48@gmail.com>
2022-11-05 12:38:02 +0100

    Created a container class named Garden with constants and constructor.

---
+++ InterviewProject/Garden.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InterviewProject
+{
+    internal class Garden
+    {
+        const int MINOBSTACLES = 2;
+        const int MAXOBSTACLES = 5;
+        const int MINOBSTACLESIZE = 5;
+        const int MAXOBSTACLESIZE = 10;
+
+        char[,] garden;
+
+        public Garden()
+        {
+            this.garden = new char[50, 50];
+        }
+
+        List<Obstacle> obstacles = new List<Obstacle>();
+
+    }
+}
