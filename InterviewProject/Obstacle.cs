ce7d22b8b900c7a092298ab4afeca26b0f8e5cea
Tamás Fodor <fodor.tamas48@gmail.com>
2022-11-05 12:22:01 +0100

    Created Obstacle class with properties and constructor.

---
+++ InterviewProject/Obstacle.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InterviewProject
+{
+    internal class Obstacle
+    {
+        int left;
+        int top;
+        int width;
+        int height;
+
+        char obstacleChar;
+
+        public int Left { get => left; set => left = value; }
+        public int Top { get => top; set => top = value; }
+        public int Width { get => width; set => width = value; }
+        public int Height { get => height; set => height = value; }
+        public char ObstacleChar { get => obstacleChar; set => obstacleChar = value; }
+
+        public Obstacle(int left, int top, int width, int height, char obstacleChar)
+        {
+            this.left = left;
+            this.top = top;
+            this.width = width;
+            this.height = height;
+            this.obstacleChar = 'X';
+        }
+    }
+}
